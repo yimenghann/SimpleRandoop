d289385fd4a73e31e3a411c27b761aab45178e78
Yimeng Han <hanyimeng2@gmail.com>
2022-04-07 20:36:54 -0500

    Add files via upload

---
+++ src/TestingRandoop.java
@@ -0,0 +1,10 @@
+package randoop;
+
+public class TestingRandoop {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+    public int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+}
